6e6d9d93494dc18d7bbc88031e141ff83953d4b7
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-21 13:19:00 +0200

    export components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
-<script setup>
-import AppHeader from './components/AppHeader.vue'
-import AppMain from './components/AppMain.vue'
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  name: 'App',
+  data(){
+    return{
+    }
+  },
+  components: {
+    AppHeader,
+    AppMain
+  }
+}
 </script>
+
 
 <template>
   <AppHeader />

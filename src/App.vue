f21f0dbe9865c756cc871c31bda07a9e780b8bdd
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-21 12:16:08 +0200

    created components

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
+
+<style>
+</style>

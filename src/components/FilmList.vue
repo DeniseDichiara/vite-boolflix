550b6c729f39df6448d912596e8da2f1d4bf6bd0
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-23 11:55:28 +0200

    added flags and data

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -10,7 +10,14 @@
             </h4>
 
             <p>
-                lang: {{ movie.original_language }} ;
+                lang: {{ movie.original_language }} 
+                {{ currentLanguage }}
+                
+                <img src="/img/en.png">
+                <img src="/img/fr.png">
+                <img src="/img/ja.png">
+                <img src="/img/pt.png">
+
                 vote: {{ movie.vote_average }}
 
             </p>
@@ -36,8 +43,16 @@ export default {
 
     },
 
+    methods: {
+
+    },
+
     data() {
         return {
+            currentLanguage: 'null',
+            availableLanguages: [
+                'it.png', 'en.png', 'fr.png', 'ja.png', 'es.png', 'pt.png'
+            ],
         }
     },
     components: {
